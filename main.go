e726acdf0d00b7196de54ffd20a4f5bc5c94f273
kabasunny <dxbqd553@yahoo.co.jp>
2024-05-29 18:35:43 +0900

    Create

--- main.go
+++ main.go
@@ -24,5 +24,6 @@ func main() {
 	r := gin.Default() //ginのデフォルトルータを初期化
 	r.GET("/items", itemController.FindAll)
 	r.GET("/items/:id", itemController.FindById)
+	r.POST("/items", itemController.Create)
 	r.Run("localhost:8080")
 }
