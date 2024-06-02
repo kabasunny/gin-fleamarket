7a47390f28711eaeb17fe2be3fe57b53c5e7690f
kabasunny <dxbqd553@yahoo.co.jp>
2024-06-02 09:25:27 +0900

    ユーザー登録用モデルの作成

--- main.go
+++ main.go
@@ -20,21 +20,29 @@ func main() {
 	// 	{ID: 2, Name: "商品2", Price: 2000, Description: "説明2", SoldOut: true},
 	// 	{ID: 3, Name: "商品3", Price: 3000, Description: "説明3", SoldOut: false},
 	// }
-
-
 	// itemRepository := repositories.NewItemMemoryRepository(items)
 	itemRepository := repositories.NewItemRepository(db) //メモリからDBへ切替
 	itemService := services.NewItemService(itemRepository)
 	itemController := controllers.NewItemController(itemService)
 
+	authRepository := repositories.NewAuthRepository(db)
+	authService := services.NewAuthService(authRepository)
+	authController := controllers.NewAuthController(authService)
+
 	r := gin.Default() //ginのデフォルトルータを初期化
+	itemRouter := r.Group("/items")
+	authRouter := r.Group("/auth")
+
 	//以下は、各リクエストにおける、エンドポイントとハンドラ関数の紐づけ設定を行っている
 	//ハンドラ関数は、実際にリクエストがあった際に発動する
-	r.GET("/items", itemController.FindAll)
-	r.GET("/items/:id", itemController.FindById)
-	r.POST("/items", itemController.Create)
-	r.PUT("/items/:id", itemController.Update)
-	r.DELETE("/items/:id", itemController.Delete)
+	itemRouter.GET("", itemController.FindAll)
+	itemRouter.GET("/:id", itemController.FindById)
+	itemRouter.POST("", itemController.Create)
+	itemRouter.PUT("/:id", itemController.Update)
+	itemRouter.DELETE("/:id", itemController.Delete)
+
+	authRouter.POST("/signup", authController.Signup)
+
 	//以下は、サーバーを立ち上げ（以降、リクエストの待機、レスポンス送信行う）
 	r.Run("localhost:8080")
 }
--- migrations/migration.go
+++ migrations/migration.go
@@ -5,12 +5,14 @@ import (
 	"gin-fleamarket/models"
 )
 
+//マイグレーションは、独立して行うため、mainで定義している
 func main() {
 	infra.Initialize() //.env ファイルから環境変数を読み込み、アプリケーションにロードするための初期化処理を行う。
 
 	db := infra.SetupDB() //データベース接続を設定し、*gorm.DB オブジェクトを返す。このオブジェクトは、データベース操作を行うためのインターフェースを提供。
 
-	if err := db.AutoMigrate(&models.Item{}); err != nil {
+	// AutoMigrate:構造体を引数として渡し、構造体に定義されているフィールドに基づいて、データベースにテーブルを作成、更新
+	if err := db.AutoMigrate(&models.Item{}, &models.User{}); err != nil {
 		panic("Failed to migrate database")
 	}
 }
